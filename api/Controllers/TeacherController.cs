46922fcc2ef08d4895121d71e0c19afa89c30746
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-08 23:04:31 +0300

    Teacher Endpoint

--- api/Controllers/TeacherController.cs
+++ api/Controllers/TeacherController.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Teacher;
+using api.models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -10,6 +14,70 @@ namespace api.Controllers
     [ApiController]
     public class TeacherController : ControllerBase
     {
-        
+        private readonly UserManager<Teacher> _userManager;
+        private readonly SignInManager<Teacher> _signInManager;
+
+        public TeacherController(UserManager<Teacher> userManager, SignInManager<Teacher> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if (user == null)
+                return BadRequest("Invalid user!");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("User not found and/or Password incorrect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                }
+            );
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var teacher = new Teacher
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email,
+                };
+
+                var createdUser = await _userManager.CreateAsync(teacher, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return Ok(
+                        new NewUserDto
+                        {
+                            UserName = teacher.UserName,
+                            Email = teacher.Email,
+                        }
+                    );
+                } else {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            } catch (Exception e) {
+                return StatusCode(500, e.Message);
+            }
+        }
     }
 }
