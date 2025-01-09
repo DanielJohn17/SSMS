5a8ecf3874efd7a4f62c3ae1a7fca30fb7e7342d
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-09 03:11:06 +0300

    Added Parent Endpoint

---
+++ api/Controllers/ParentController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Teacher;
+using api.models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [Route("api/teacher")]
+    [ApiController]
+    public class ParentController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public ParentController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if (user == null)
+                return BadRequest("Invalid user!");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("User not found and/or Password incorrect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                }
+            );
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var parent = new Parent
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email,
+                };
+
+                var createdUser = await _userManager.CreateAsync(parent, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return Ok(
+                        new NewUserDto
+                        {
+                            UserName = parent.UserName,
+                            Email = parent.Email,
+                        }
+                    );
+                } else {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            } catch (Exception e) {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
