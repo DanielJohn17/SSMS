4665dcbb37321c8a840f0e8504067a7df983a1f1
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-09 05:24:42 +0300

    Added teacher-section endpoint

--- api/models/TeacherSection.cs
+++ api/models/TeacherSection.cs
@@ -10,8 +10,8 @@ namespace api.models
     [Table("TeacherSections")]
     public class TeacherSection
     {
-        public string? TeacherId { get; set; }
-        public int? SectionId { get; set; }
+        public string TeacherId { get; set; } = string.Empty;
+        public int SectionId { get; set; }
         public Teacher? Teacher { get; set; }
         public Section? Section { get; set; }
     }
